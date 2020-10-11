7eba2df2004c53e59e5d4e42114df6d94e0eb5c8
Youzhi Hu <jyzhu0101@gmail.com>
2020-10-11 15:41:17 +1100

    Created Save page.

---
+++ client/src/pages/Save.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box } from "@material-ui/core"
+
+
+const Save = () => {
+    return (
+    <Box container="true" justify='center'>
+    </Box>
+    )
+}
+
+export default Save
