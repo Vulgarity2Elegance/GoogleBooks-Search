119373a669ae0d3497180aaed3edee16e614624c
Youzhi Hu <jyzhu0101@gmail.com>
2020-10-11 15:40:52 +1100

    Created Search page.

---
+++ client/src/pages/Search.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Box } from "@material-ui/core"
+
+
+
+const Search = () => {
+    return (
+        <Box container="true" justify='center'>
+        </Box>
+    )
+}
+
+export default Search
