b579229b9846123c991d923a7d0b1f4f933a208b
Youzhi Hu <jyzhu0101@gmail.com>
2020-10-15 20:13:26 +1100

    Created Book.js component by desconstructing props.

---
+++ client/src/components/Book.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react'
+import { Card, CardActions, CardContent, CardHeader, CardMedia, IconButton, Typography } from '@material-ui/core'
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import InfoIcon from '@material-ui/icons/Info';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { makeStyles } from '@material-ui/core/styles';
+import Collapse from '@material-ui/core/Collapse';
+import clsx from 'clsx';
+
+function Book(props) {
+
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            maxWidth: '100%',
+            flexGrow: 1,
+            backgroundColor: 'tan'
+        },
+        expand: {
+            transform: 'rotate(0deg)',
+            marginLeft: 'auto',
+            transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+          }),
+        },
+        expandOpen: {
+            transform: 'rotate(180deg)',
+        },
+        image: {
+            display: 'block',
+            margin: 'auto',
+            width: 128,
+            height: 196
+        }
+      }));
+
+    const classes = useStyles()
+    const [expanded, setExpanded] = useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+      };
+
+    const {title, subtitle, authors, picture, description, url} = props;
+
+    return(
+    
+        <Card className={classes.root}>
+            <CardMedia component='img' className={classes.image} image={picture} title={title} />
+            <CardHeader
+            title={title}
+            subheader={subtitle}/>
+            <CardContent variant='caption'>
+                {authors}
+            </CardContent>
+            <CardActions>
+                <IconButton aria-label='save'>
+                    <FavoriteIcon />
+                </IconButton>
+                <IconButton aria-label='info' href={url} target='_blank'>
+                    <InfoIcon/>
+                </IconButton>
+                <IconButton
+                className={clsx(classes.expand, {
+                    [classes.expandOpen]: expanded,
+                })}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show more"
+                >
+                <ExpandMoreIcon />
+                </IconButton>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                    <Typography style={{color: '#16264c'}} variant="body2" color="textSecondary" component="p">{description}</Typography>
+                </CardContent>
+            </Collapse>
+        </Card>
+    )
+}
+
+export default Book
