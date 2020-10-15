16da59259c082486d92ca55a25633ed0c9ade1ec
Youzhi Hu <jyzhu0101@gmail.com>
2020-10-15 20:19:15 +1100

    Can search books and render results through Google API.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,26 +2,35 @@ import React, { useState } from 'react'
 import { Box } from '@material-ui/core'
 import Search from "../components/Search"
 import {API} from "../utils/API"
+import Results from "../components/Results"
 
 function Home() {
+    const [bookState, setBookState] =useState([]);
+
     const [query, setQuery] = useState("")
     const handleInputChange = e => {setQuery(e.target.value)}
 
     const handleFormSubmit = async e => {
         e.preventDefault()
         setQuery('')
-        const result = await API.getBooks(query)
-        console.log(result)
+        const results = await API.getBooks(query)
+        const books = results.data.items
+        if (books) {
+            setBookState(books)
+        }
+        console.log(books)
     }
 
-    return (
+    return (   
         <Box component='div'>
             <Search 
             handleFormSubmit={handleFormSubmit}
             handleInputChange={handleInputChange}
             query={query}
             />
+            <Results books={bookState}/>
         </Box>
+
     )
 }
 
